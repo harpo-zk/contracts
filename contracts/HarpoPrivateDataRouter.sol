c17eb03d8c422ff2e46a0adacebf9fe6a2289f34
Alex Costa <alexcostars@gmail.com>
2025-05-01 21:20:04 -0300

    adicionando o validador ZKP para verifição de provas onde o PrivateData foi submetido com 3 destinatários. Implementação para o PR https://github.com/harpo-zk/harpo/pull/12

--- contracts/HarpoPrivateDataRouter.sol
+++ contracts/HarpoPrivateDataRouter.sol
@@ -1,22 +1,36 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
 struct HarpoPrivateData {
     bytes32 cipher;
     bytes32[2] c1;
 }
-struct HarpoPrivateDataProof {
+
+struct HarpoPrivateDataProof2pk {
     uint[2] a;
     uint[2][2] b;
     uint[2] c;
     uint[2] input;
 }
-struct HarpoPrivateParameter {
-    HarpoPrivateData[2] data;
-    HarpoPrivateDataProof proof;
+
+struct HarpoPrivateDataProof3pk {
+    uint[2] a;
+    uint[2][2] b;
+    uint[2] c;
+    uint[3] input;
 }
 
-interface HarpoPrivateDataVerifier {
+struct HarpoPrivateParameter2pk {
+    HarpoPrivateData[2] data;
+    HarpoPrivateDataProof2pk proof;
+}
+
+struct HarpoPrivateParameter3pk {
+    HarpoPrivateData[3] data;
+    HarpoPrivateDataProof3pk proof;
+}
+
+interface HarpoPrivateDataVerifier2pk {
     function verifyProof(
         uint[2] memory a,
         uint[2][2] memory b,
@@ -25,19 +39,41 @@ interface HarpoPrivateDataVerifier {
     ) external view returns (bool r);
 }
 
+interface HarpoPrivateDataVerifier3pk {
+    function verifyProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[3] memory input
+    ) external view returns (bool r);
+}
+
 contract HarpoPrivateDataRouter {
 
-    HarpoPrivateDataVerifier public harpoPrivateDataVerifier;
+    HarpoPrivateDataVerifier2pk public harpoPrivateDataVerifier2pk;
+    HarpoPrivateDataVerifier3pk public harpoPrivateDataVerifier3pk;
 
-    constructor(address harpoPrivateDataVerifierAddress) {
-        harpoPrivateDataVerifier = HarpoPrivateDataVerifier(harpoPrivateDataVerifierAddress);
+    constructor(address harpoPrivateDataVerifier2pkAddress, address harpoPrivateDataVerifier3pkAddress) {
+        harpoPrivateDataVerifier2pk = HarpoPrivateDataVerifier2pk(harpoPrivateDataVerifier2pkAddress);
+        harpoPrivateDataVerifier3pk = HarpoPrivateDataVerifier3pk(harpoPrivateDataVerifier3pkAddress);
     }
 
-    function verifyProof(HarpoPrivateParameter calldata data) public view {
+    function verifyProof(HarpoPrivateParameter2pk calldata data) public view {
         require(data.data[0].cipher == bytes32(data.proof.input[0]), "invalid harpo proof");
         require(data.data[1].cipher == bytes32(data.proof.input[1]), "invalid harpo proof");
 
-        bool v = harpoPrivateDataVerifier.verifyProof(data.proof.a, data.proof.b, data.proof.c, data.proof.input);
+        bool v = harpoPrivateDataVerifier2pk.verifyProof(data.proof.a, data.proof.b, data.proof.c, data.proof.input);
+        if(!v) {
+            revert("invalid harpo proof");
+        }
+    }
+
+    function verifyProof(HarpoPrivateParameter3pk calldata data) public view {
+        require(data.data[0].cipher == bytes32(data.proof.input[0]), "invalid harpo proof");
+        require(data.data[1].cipher == bytes32(data.proof.input[1]), "invalid harpo proof");
+        require(data.data[2].cipher == bytes32(data.proof.input[2]), "invalid harpo proof");
+
+        bool v = harpoPrivateDataVerifier3pk.verifyProof(data.proof.a, data.proof.b, data.proof.c, data.proof.input);
         if(!v) {
             revert("invalid harpo proof");
         }
